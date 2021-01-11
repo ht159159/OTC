80ab7a69c30af27a5aad5a4db35a0a285c680893
Sheep <ht159159@gmail.com>
2021-01-11 10:52:22 +0800

    first commit02

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/OTC/" : "/",
 	lintOnSave: false,
     chainWebpack: config => {
         if (process.env.NODE_ENV === "development") {
